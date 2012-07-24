d6be9b6f8b52c1591313366658d4db700c020330
Trevor Bramwell <tehriddler@gmail.com>
2012-07-24 07:30:01 -0700

    Environment setup

---
+++ tests/dynamicform.js
@@ -0,0 +1,1 @@
+../dynamicform/dynamicform.js
---
+++ tests/tests.js
@@ -0,0 +1,7 @@
+module( "DynamicForm - Core" );
+test( "init function", function() {
+   var $fixture = $( "qunit-fixture" );
+
+   ok( true, "message about test" );
+   deepEqual( 0, 0, "0 deepEquals check" );
+});
